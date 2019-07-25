1b3faa243a1e5f801dc457379dccad085db442d8
A Usoro <aolausoro@gmail.com>
2019-07-26 00:58:04 +0100

    Added UI, API Integration and Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import Alert from './components/layout/Alert';
+import PrivateRoute from './components/Routing/PrivateRoute';
+
+import AuthState from './context/auth/AuthState';
+import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './Utils/setAuthToken';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+if (localStorage.token) {
+  setAuthToken(localStorage.token)
 }
+
+const App = () => {
+  return (
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alert />
+                <Switch>
+                  <PrivateRoute exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
+  );
+};
 
 export default App;
